43e530a2cd0857179b68e4e2dbb71b7997e8156b
Jason Chambers <jason.d.chambers@gmail.com>
2025-08-18 17:31:06 -0600

    Disable flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,8 +1,9 @@
 return {
-  {
-    "folke/flash.nvim",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, false },
-    },
-  },
+  --  Dissabling flash.vim {
+  --    "folke/flash.nvim",
+  --    keys = {
+  --      { "s", mode = { "n", "x", "o" }, false },
+  --    },
+  --  },
 }
+
